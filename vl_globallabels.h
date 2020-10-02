0863898e1565b12112024fad24e576c4dbbe400e
Andreas Müller <schnitzeltony@gmail.com>
2020-10-02 10:06:56 +0200

    Introduce VLGlobalLabels for labels used commonly
    Since this is nothing for public use, get rid of header glob magic in
    CMakeLists.txt.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ vl_globallabels.h
@@ -0,0 +1,14 @@
+#ifndef VL_GLOBALLABELS_H
+#define VL_GLOBALLABELS_H
+
+#include "globalIncludes.h"
+
+class VLGlobalLabels {
+public:
+    const static QString allComponentsName() { return QStringLiteral("__ALL_COMPONENTS__"); }
+    const static QStringList noStoreComponents() { return QStringList()
+        << QStringLiteral("EntityName")
+        << QStringLiteral("INF_ModuleInterface"); }
+};
+
+#endif // VL_GLOBALLABELS_H
