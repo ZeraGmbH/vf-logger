9a189ed5bf8cacfb54847bfcc938c46c9f8b89b3
bhamacher <b.hamacher@zera.de>
2020-10-08 14:41:31 +0200

    Fixed sql shema
    Wrong reference to Foreign keys entities and components in TABLE valuemap
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- sqlite/schema_sqlite.sql
+++ sqlite/schema_sqlite.sql
@@ -3,7 +3,7 @@ CREATE TABLE entities (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, entity_nam
 CREATE TABLE records (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, record_name varchar(255) NOT NULL);
 CREATE TABLE transactions (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, recordsid integer(10) NOT NULL, transaction_name varchar(255), contentset_names varchar(255), guicontext_name varchar(255), start_time timestamp, stop_time timestamp, FOREIGN KEY(recordsid) REFERENCES records(id));
 CREATE TABLE transactions_valuemap (transactionsid integer(10) NOT NULL, valueid integer(10) NOT NULL, PRIMARY KEY (transactionsid, valueid), FOREIGN KEY(valueid) REFERENCES valuemap(id), FOREIGN KEY(transactionsid) REFERENCES transactions(id));
-CREATE TABLE valuemap (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, value_timestamp timestamp, component_value numeric(19, 0), componentid integer(10), entityiesid integer(10), FOREIGN KEY(entityiesid) REFERENCES enties(id), FOREIGN KEY(componentid) REFERENCES component(id));
+CREATE TABLE valuemap (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, value_timestamp timestamp, component_value numeric(19, 0), componentid integer(10), entityiesid integer(10), FOREIGN KEY(entityiesid) REFERENCES entities(id), FOREIGN KEY(componentid) REFERENCES components(id));
 
 /*CREATE VIEW valueview AS SELECT records.record_name, transactions.transaction_name, enties.entity_name, component.component_name, valuemap.value_timestamp, valuemap.component_value FROM records INNER JOIN transactions ON records.id = transactions.recordsid INNER JOIN valuemap ON transactions.id = valuemap.transactionid INNER JOIN enties ON valuemap.* = enties.id INNER JOIN component ON valuemap.componentid = component.id; */
 
