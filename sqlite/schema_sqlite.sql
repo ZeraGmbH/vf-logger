46aab852399198760e6338231ebda62d3e73af37
Andreas Müller <schnitzeltony@gmail.com>
2024-03-18 17:41:19 +0100

    schema_sqlite.sql: Make human/dBizzy readable
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- sqlite/schema_sqlite.sql
+++ sqlite/schema_sqlite.sql
@@ -1,33 +1,50 @@
-CREATE TABLE components (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, component_name varchar(255));
-CREATE TABLE entities (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, entity_name varchar(255));
-CREATE TABLE sessions (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, session_name varchar(255) NOT NULL);
-CREATE TABLE transactions (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, sessionid integer(10) NOT NULL, transaction_name varchar(255), contentset_names varchar(255), guicontext_name varchar(255), start_time timestamp, stop_time timestamp, FOREIGN KEY(sessionid) REFERENCES sessions(id));
-CREATE TABLE transactions_valuemap (transactionsid integer(10) NOT NULL, valueid integer(10) NOT NULL, PRIMARY KEY (transactionsid, valueid), FOREIGN KEY(valueid) REFERENCES valuemap(id), FOREIGN KEY(transactionsid) REFERENCES transactions(id));
-CREATE TABLE sessions_valuemap (sessionsid integer(10) NOT NULL, valueid integer(10) NOT NULL, PRIMARY KEY (sessionsid, valueid), FOREIGN KEY(valueid) REFERENCES valuemap(id), FOREIGN KEY(sessionsid) REFERENCES sessions(id));
-CREATE TABLE valuemap (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, value_timestamp timestamp, component_value numeric(19, 0), componentid integer(10), entityiesid integer(10), FOREIGN KEY(entityiesid) REFERENCES entities(id), FOREIGN KEY(componentid) REFERENCES components(id));
+CREATE TABLE components (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    component_name varchar(255)
+    );
 
-/*
-SELECT
-        sessions.session_name,
-        transactions.transaction_name,
-        entities.entity_name,
-        components.component_name,
-        valuemap.value_timestamp,
-        valuemap.component_value
-FROM sessions
-INNER JOIN transactions ON sessions.id = transactions.sessionid
-INNER JOIN transactions_valuemap ON transactions.id = transactions_valuemap.transactionsid
-INNER JOIN valuemap ON valuemap.id = transactions_valuemap.valueid
-INNER JOIN entities ON valuemap.entityiesid = entities.id
-INNER JOIN components ON valuemap.componentid = components.id
-ORDER BY valuemap.value_timestamp
+CREATE TABLE entities (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    entity_name varchar(255)
+    );
 
-/* shows the data in a human readable / CSV processable form */;
-/* example output:
-      value_timestamp       | session_name |transaction_name |entity_name | component_name | component_value
-----------------------------+--------------+-------------+----------------+--------------------
- 2016-10-06 11:58:34.504319 |     default  | someName        | someEntity |  someComponent | 2.3536
- 2016-10-06 11:58:34.529059 |     default  | someName        | someEntity |  someComponent | some string
- 2016-10-06 11:58:34.553821 |     default  | someName        | someEntity |  someComponent | {9.2453,36.436347}
-*/;
+CREATE TABLE sessions (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    session_name varchar(255) NOT NULL);
+
+CREATE TABLE transactions (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    sessionid integer(10) NOT NULL,
+    transaction_name varchar(255),
+    contentset_names varchar(255),
+    guicontext_name varchar(255),
+    start_time timestamp,
+    stop_time timestamp,
+    FOREIGN KEY (sessionid) REFERENCES sessions(id)
+    );
+
+CREATE TABLE transactions_valuemap (
+    transactionsid integer(10) NOT NULL,
+    valueid integer(10) NOT NULL,
+    PRIMARY KEY (transactionsid, valueid),
+    FOREIGN KEY (valueid) REFERENCES valuemap(id),
+    FOREIGN KEY (transactionsid) REFERENCES transactions(id)
+    );
+
+CREATE TABLE sessions_valuemap (
+    sessionsid integer(10) NOT NULL,
+    valueid integer(10) NOT NULL,
+    PRIMARY KEY (sessionsid, valueid),
+    FOREIGN KEY (valueid) REFERENCES valuemap(id),
+    FOREIGN KEY (sessionsid) REFERENCES sessions(id));
+
+CREATE TABLE valuemap (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    value_timestamp timestamp,
+    component_value numeric(19, 0),
+    componentid integer(10),
+    entityiesid integer(10),
+    FOREIGN KEY (entityiesid) REFERENCES entities(id),
+    FOREIGN KEY (componentid) REFERENCES components(id)
+    );
 
