ac1d3f89c385b40b3b3cd42d87c5ac7062e6d2fe
Andreas Müller <schnitzeltony@gmail.com>
2024-04-13 00:52:10 +0200

    Initial work on content set tests
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ tests/test_contentsets.h
@@ -0,0 +1,26 @@
+#ifndef TEST_CONTENTSETS_H
+#define TEST_CONTENTSETS_H
+
+#include "vsc_scriptsystem.h"
+#include "veinqml.h"
+#include <zeradblogger.h>
+#include <testveinserver.h>
+#include <QObject>
+#include <memory>
+
+class test_contentsets : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void checkEntities();
+private:
+    void setupServer();
+    std::unique_ptr<TestVeinServer> m_server;
+    VeinEvent::StorageSystem* m_storage;
+    std::unique_ptr<VeinScript::ScriptSystem> m_scriptSystem;
+    std::unique_ptr<VeinApiQml::VeinQml> m_qmlSystem;
+    std::unique_ptr<ZeraDBLogger> m_dataLoggerSystem;
+};
+
+#endif // TEST_CONTENTSETS_H
