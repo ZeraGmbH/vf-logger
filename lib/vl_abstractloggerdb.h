0f791b043a352d08d94359afea639ca76077234c
Andreas Müller <schnitzeltony@gmail.com>
2024-04-17 15:51:18 +0200

    vl_abstractloggerdb.h: Remove noise - this interface is huge
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- lib/vl_abstractloggerdb.h
+++ lib/vl_abstractloggerdb.h
@@ -13,28 +13,24 @@ namespace VeinLogger
 class AbstractLoggerDB : public QObject
 {
     Q_OBJECT
-
 public:
     AbstractLoggerDB(QObject *t_parent=nullptr);
-    virtual ~AbstractLoggerDB()
-    {
-
-    }
+    virtual ~AbstractLoggerDB() = default;
 
     enum class STORAGE_MODE : int {
         TEXT = 0,
         BINARY = 1,
     };
 
-    virtual bool hasEntityId(int t_entityId) const =0;
-    virtual bool hasComponentName(const QString &t_componentName) const =0;
-    virtual bool hasSessionName(const QString &t_sessionName) const =0;
+    virtual bool hasEntityId(int t_entityId) const = 0;
+    virtual bool hasComponentName(const QString &t_componentName) const = 0;
+    virtual bool hasSessionName(const QString &dbSessionName) const = 0;
 
-    virtual bool databaseIsOpen() const =0;
-    virtual QString databasePath() const =0;
-    virtual void setStorageMode(STORAGE_MODE t_storageMode) =0;
-    virtual STORAGE_MODE getStorageMode() const =0;
-    virtual std::function<bool(QString)> getDatabaseValidationFunction() const =0;
+    virtual bool databaseIsOpen() const = 0;
+    virtual QString databasePath() const = 0;
+    virtual void setStorageMode(STORAGE_MODE t_storageMode) = 0;
+    virtual STORAGE_MODE getStorageMode() const = 0;
+    virtual std::function<bool(QString)> getDatabaseValidationFunction() const = 0;
 
 signals:
     void sigDatabaseError(const QString &t_errorString);
@@ -42,37 +38,22 @@ signals:
     void sigNewSessionList(QStringList p_sessions);
 
 public slots:
-    virtual void initLocalData() =0;
-    virtual void addComponent(const QString &t_componentName) =0;
-    virtual void addEntity(int t_entityId, QString t_entityName) =0;
-    virtual int addTransaction(const QString &t_transactionName, const QString &t_sessionName, const QString &t_contentSets, const QString &t_guiContextName) =0;
-    /**
-     * @brief addStartTime
-     * @param t_transactionId: sql transaction id
-     * @param t_time: transaction start time
-     * @return true
-     *
-     * set the snapshot time or recording start time
-     */
+    virtual void initLocalData() = 0;
+    virtual void addComponent(const QString &t_componentName) = 0;
+    virtual void addEntity(int t_entityId, QString t_entityName) = 0;
+    virtual int addTransaction(const QString &t_transactionName, const QString &dbSessionName, const QString &t_contentSets, const QString &t_guiContextName) = 0;
+
     virtual bool addStartTime(int t_transactionId, QDateTime t_time) = 0;
-    /**
-     * @brief addStopTime
-     * @param t_transactionId: sql transaction id
-     * @param t_time: transaction stop time
-     * @return true
-     *
-     * set the snapshot time or recording stop time.
-     */
     virtual bool addStopTime(int t_transactionId,  QDateTime t_time) = 0;
 
-    virtual QJsonDocument readTransaction(const QString &p_transaction, const QString &p_session) = 0;
-    virtual QVariant readSessionComponent(const QString &p_session, const QString &p_entity, const QString &p_component) = 0;
-    virtual int addSession(const QString &t_sessionName,QList<QVariantMap> p_staticData) =0;
-    virtual bool deleteSession(const QString &t_session) = 0;
-    virtual void addLoggedValue(const QString &t_sessionName, QVector<int> t_transactionIds, int t_entityId, const QString &t_componentName, QVariant t_value, QDateTime t_timestamp) =0;
+    virtual QJsonDocument readTransaction(const QString &transactionName, const QString &dbSessionName) = 0;
+    virtual QVariant readSessionComponent(const QString &dbSessionName, const QString &entityName, const QString &componentName) = 0;
+    virtual int addSession(const QString &dbSessionName,QList<QVariantMap> componentValuesStoredOncePerSession) =0;
+    virtual bool deleteSession(const QString &sessionName) = 0;
+    virtual void addLoggedValue(const QString &dbSessionName, QVector<int> t_transactionIds, int t_entityId, const QString &t_componentName, QVariant t_value, QDateTime t_timestamp) = 0;
 
-    virtual bool openDatabase(const QString &t_dbPath) =0;
-    virtual void runBatchedExecution() =0;
+    virtual bool openDatabase(const QString &t_dbPath) = 0;
+    virtual void runBatchedExecution() = 0;
 };
 
 /// @b factory function alias to create database
