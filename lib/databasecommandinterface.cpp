6cffabbdd025d0fea02df55543e30433065c3289
Andreas Müller <schnitzeltony@gmail.com>
2024-05-01 23:08:18 +0200

    DatabaseCommandInterface: Adjust namespace usage
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- lib/databasecommandinterface.cpp
+++ lib/databasecommandinterface.cpp
@@ -1,11 +1,14 @@
 #include "databasecommandinterface.h"
 #include "vl_abstractloggerdb.h"
 
-void VeinLogger::DatabaseCommandInterface::connectDb(AbstractLoggerDB *db)
+namespace VeinLogger
+{
+void DatabaseCommandInterface::connectDb(AbstractLoggerDB *db)
 {
     connect(this, &DatabaseCommandInterface::sigAddLoggedValue, db, &AbstractLoggerDB::addLoggedValue);
     connect(this, &DatabaseCommandInterface::sigAddEntity, db, &AbstractLoggerDB::addEntity);
     connect(this, &DatabaseCommandInterface::sigAddComponent, db, &AbstractLoggerDB::addComponent);
     connect(this, &DatabaseCommandInterface::sigAddSession, db, &AbstractLoggerDB::addSession);
     connect(this, &DatabaseCommandInterface::sigOpenDatabase, db, &AbstractLoggerDB::openDatabase);
+}
 }
