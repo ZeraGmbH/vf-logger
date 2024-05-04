a7c79619289bf1adb36cbd87ce17afe90e28519f
Andreas Müller <schnitzeltony@gmail.com>
2024-05-04 02:38:57 +0200

    DatabaseLogger::onOpenDatabase: Simplify database change
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- lib/vl_databaselogger.h
+++ lib/vl_databaselogger.h
@@ -59,7 +59,7 @@ private:
     void addValueToDb(const QVariant newValue, const int entityId, const QString componentName);
     void writeCurrentStorageToDb();
     QStringList getComponentsFilteredForDb(int entityId);
-    void terminateDbThread();
+    void terminateCurrentDb();
 
     DataLoggerPrivate *m_dPtr = nullptr;
     int m_entityId;
