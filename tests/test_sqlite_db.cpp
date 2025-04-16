d033cbf0eae261b4a95cf960652aea6451b8597d
amutalik <a.mutalik@zera.de>
2025-04-16 16:29:38 +0200

    test_sqlite_db: Test 'displaySessionInfo' using 'RPC_displaySessionsInfos'
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- tests/test_sqlite_db.cpp
+++ tests/test_sqlite_db.cpp
@@ -80,25 +80,27 @@ void test_sqlite_db::logInsertsEntityComponents()
 
 void test_sqlite_db::displaySessionInfo()
 {
+    QString sessionName = "Session1";
+    QString transactionName = "Transaction1";
+
     m_testSystem.setupServer();
     m_testSystem.loadDatabase();
-    m_testSystem.setComponent(dataLoggerEntityId, "sessionName", "Session1");
+    m_testSystem.setComponent(dataLoggerEntityId, "sessionName", sessionName);
     m_testSystem.setComponent(dataLoggerEntityId, "guiContext", QVariantList() << "ZeraGuiActualValues");
     m_testSystem.setComponent(dataLoggerEntityId, "currentContentSets", QVariantList() << "ZeraAll");
-
-    m_testSystem.startLogging();
+    m_testSystem.startLogging("Session1", transactionName);
     m_testSystem.stopLogging();
-    QString transactionName = m_testSystem.getValueOfComponent(dataLoggerEntityId, "transactionName").toString();
-    QStringList guiContexts = m_testSystem.getValueOfComponent(dataLoggerEntityId, "guiContext").toStringList();
-    QString guiContext = guiContexts[0];
-    QStringList contentSets = m_testSystem.getValueOfComponent(dataLoggerEntityId, "currentContentSets").toStringList();
-    QString contentSet = contentSets[0];
 
     QFile file(":/sqlite-inserts/dumpSessionInfos.json");
     QVERIFY(file.open(QFile::ReadOnly));
     QByteArray jsonExpected = file.readAll();
 
-    QJsonObject jsonObj = m_testSystem.displaySessionsInfos("Session1", transactionName, guiContext, contentSet);
+    QJsonObject jsonObj = m_testSystem.displaySessionsInfos(sessionName);
+    //Remove 'Time' information as it indicates the actual time when logging was done. So it will never match with 'Time' from jsonExpected.
+    QJsonObject temp = jsonObj.value(transactionName).toObject();
+    temp.insert("Time", QString());
+    jsonObj.insert(transactionName, temp);
+
     QJsonDocument jsonDoc(jsonObj);
     QString jsonDumped = jsonDoc.toJson(QJsonDocument::Indented);
     QVERIFY(TestLogHelpers::compareAndLogOnDiff(jsonExpected, jsonDumped));
